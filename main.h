b3032a8c40cce301817b852c3e84d0951838b739
Bentezzy <64396883+bentezzy@users.noreply.github.com>
2023-04-25 22:46:16 +0300

    Update main.h

--- main.h
+++ main.h
@@ -34,7 +34,7 @@ struct fmt
 /**
  * typedef struct fmt fmt_t - Struct op
  *
- * @fmt: The format.
+ * @fmt: The format spec.
  * @fm_t: The function associated.
  */
 typedef struct fmt fmt_t;
@@ -70,7 +70,7 @@ int print_hexa_upper(va_list types, char buffer[],
 int print_hexa(va_list types, char map_to[],
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
-/* Function to print non printable characters */
+/* Function to print the non printable characters */
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
